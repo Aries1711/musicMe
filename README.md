a865142fb5ef44c6713818b8dafb1f66239dcaee
Arisqi Setyawan <levfreey17@gmail.com>
2022-09-03 13:42:42 +0700

    write some doc and change icon launcher

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
-# music_app_challenge
+# MusicMe
 
-A new Flutter project.
+Simple music player using flutter, Music from Itunes.
 
-## Getting Started
+## Features
 
-This project is a starting point for a Flutter application.
+1. Will code search your favourite artist/song in iTunes!, play & enjoy!
+2. Pause, prev, next & seek on duration of your song.
+3. Search other song while still listening to the current song or just push button next or prev (vice verca) to play song from the list.
+4. Error handling api, empty search and many more.
 
-A few resources to get you started if this is your first Flutter project:
+## Developt or build App requirements
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- Graddle version gradle-6.7-all.zip
+- installed Android studio and Flutter
+- Flutter v3.3.0 on channel stable
+- Dart verrsion v2.18.0
+- Simulator or Android device minSdk 23 [marshmallow](https://androidsdkoffline.blogspot.com/p/android-sdk-60-api-23-marshmallow.html) you can just download it from your android studio.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Supported Device
+
+-Simulator or Android device minSdk 23 [marshmallow]
+-Ios OS IPhone 9.0
+
+## Instruction Deploy in your local environment
+
+1. Clone from this repository by download zip or https or ssh (recommended using https or ssh)
+   - Copy repository url
+   - Open your fav code editor _(Recommended using Android Studio or Visual Studio Code)_
+   - _(Android Studio)_ New -> Project from Version Control.. -> Paste the url, click OK
+   - _(Visual Studio Code)_ create folder where do you want, open terminal and paste url clone then enter -> open root folder.
+2. Run "flutter pub get" in the project directory or click the highlighted instruction in Android Studio or Visual Studio Code
+3. Prepare the Android Virtual Device or real device _(Make sure its supported device)_
+4. Run main.dart
+
+## Project Code Infrastructure & Design Pattern
+
+This App project directory consist of 5 directories (Preparation for preparation for the next possible complex feature & scallable code)
+
+# Design pattern or state management used
+
+- BLOC (State Management)
+- Design Pattern (Repository Pattern)
+
+# Folder Structure & Description
--- lib/presentations/screens/home_screens.dart
+++ lib/presentations/screens/home_screens.dart
@@ -55,174 +55,182 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: colorBlack,
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: AppbarSearch(
-                size: size,
-                searchTextController: searchController,
-                onChanged: (value) {},
-                onSubmitted: (value) async {
-                  await blocMusicData.getMusicDataByQuery(value);
-                },
-                onPressSearch: () async {
-                  await blocMusicData
-                      .getMusicDataByQuery(searchController.text);
-                },
-              ),
-            ),
-            Expanded(
-              flex: 9,
-              child: Container(
-                child: BlocConsumer<MusicPlayerCubit, MusicPlayerState>(
-                  bloc: blocMusicPlayer,
-                  listener: (context, state) async {
-                    if (state is MusicPlayerPlaying) {
-                      await _player
-                          .play(UrlSource(state.musicData.trackPreview));
-                      _player.onDurationChanged.listen((Duration p) {
-                        setState(() {
-                          _musicLength = p;
-                        });
-                      });
-                      _player.onPositionChanged.listen((Duration p) {
-                        setState(() {
-                          _position = p;
-                        });
-                      });
-                      _player.onPlayerComplete.listen((event) {
-                        blocMusicPlayer.completePlay();
-                      });
-                    }
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: colorBlack,
+          body: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: AppbarSearch(
+                  size: size,
+                  searchTextController: searchController,
+                  onChanged: (value) {},
+                  onSubmitted: (value) async {
+                    await blocMusicData.getMusicDataByQuery(value);
                   },
-                  builder: (context, state) {
-                    return Stack(
-                      children: <Widget>[
-                        BlocBuilder<MusicDataCubit, MusicDataState>(
-                          bloc: blocMusicData,
-                          buildWhen: (previus, current) =>
-                              previus.musicData != current.musicData,
-                          builder: (context, stateData) {
-                            switch (stateData.musicData.status) {
-                              case Status.INITIAL:
-                                return WidgetStateHandling(
-                                  assetsPath: "assets/images/podcast.png",
-                                  message: "Let's start find songs",
-                                );
-                              case Status.LOADING:
-                                return WidgetStateHandling(
-                                  assetsPath: "assets/images/podcast.png",
-                                  message: "Please wait...",
-                                );
-                              case Status.COMPLETED:
-                                final data = stateData.musicData.data ?? [];
-                                if (data.isNotEmpty) {
-                                  return ListView.builder(
-                                    itemCount: data.length,
-                                    controller: _scrollController,
-                                    itemBuilder: (context, index) {
-                                      return ListMusicData(
-                                        indexMusic: index,
-                                        activePlaying: (() {
-                                          if (state is MusicPlayerPlaying &&
-                                              state.musicData.trackPreview ==
-                                                  stateData
-                                                      .musicData
-                                                      .data![index]
-                                                      .trackPreview) {
-                                            return true;
-                                          } else {
-                                            return false;
-                                          }
-                                        }()),
-                                        dataMusic:
-                                            stateData.musicData.data![index],
-                                        onPressSongSelected: () =>
-                                            blocMusicPlayer.startPlay(
-                                                stateData
-                                                    .musicData.data![index],
-                                                stateData.musicData.data!,
-                                                index),
-                                      );
-                                    },
-                                  );
-                                } else {
-                                  return WidgetStateHandling(
-                                      assetsPath:
-                                          "assets/images/error_data.png",
-                                      message: "Sorry we can find anything..");
-                                }
-                              case Status.ERROR:
-                                return WidgetStateHandling(
-                                    assetsPath: "assets/images/error_data.png",
-                                    message: stateData.musicData.message!);
-                              default:
-                                return WidgetStateHandling(
-                                  assetsPath: "assets/images/podcast.png",
-                                  message: "Let's start find songs",
-                                );
-                            }
-                          },
-                        ),
-                        (() {
-                          if (state is MusicPlayerPlaying) {
-                            return BottomPlayerSheet(
-                                position: _position,
-                                musicLength: _musicLength,
-                                onPressNext: () {
-                                  if (state.indexData <
-                                      state.musicDataList.length) {
-                                    blocMusicPlayer.startPlay(
-                                        state
-                                            .musicDataList[state.indexData + 1],
-                                        state.musicDataList,
-                                        state.indexData + 1);
-                                  }
-                                },
-                                onPressPrevius: () {
-                                  if (state.indexData >= 1) {
-                                    blocMusicPlayer.startPlay(
-                                        state
-                                            .musicDataList[state.indexData - 1],
-                                        state.musicDataList,
-                                        state.indexData - 1);
-                                  }
-                                },
-                                musicDataList: state.musicDataList,
-                                indexData: state.indexData,
-                                playingMusic: playingMusic,
-                                onPressPause: () {
-                                  if (playingMusic) {
-                                    _player.resume();
-                                  } else {
-                                    _player.pause();
-                                  }
-                                  setState(() {
-                                    playingMusic = !playingMusic;
-                                  });
-                                },
-                                musicData: state.musicData,
-                                onSeekMusic: (value) {
-                                  int valueInt = value.round();
-                                  Duration newPos = Duration(seconds: valueInt);
-                                  _player.seek(newPos);
-                                });
-                          } else {
-                            return Container();
-                          }
-                        }())
-                      ],
-                    );
+                  onPressSearch: () async {
+                    await blocMusicData
+                        .getMusicDataByQuery(searchController.text);
                   },
                 ),
               ),
-            )
-          ],
+              Expanded(
+                flex: 9,
+                child: Container(
+                  child: BlocConsumer<MusicPlayerCubit, MusicPlayerState>(
+                    bloc: blocMusicPlayer,
+                    listener: (context, state) async {
+                      if (state is MusicPlayerPlaying) {
+                        await _player
+                            .play(UrlSource(state.musicData.trackPreview));
+                        _player.onDurationChanged.listen((Duration p) {
+                          setState(() {
+                            _musicLength = p;
+                          });
+                        });
+                        _player.onPositionChanged.listen((Duration p) {
+                          setState(() {
+                            _position = p;
+                          });
+                        });
+                        _player.onPlayerComplete.listen((event) {
+                          blocMusicPlayer.completePlay();
+                        });
+                      }
+                    },
+                    builder: (context, state) {
+                      return Stack(
+                        children: <Widget>[
+                          BlocBuilder<MusicDataCubit, MusicDataState>(
+                            bloc: blocMusicData,
+                            buildWhen: (previus, current) =>
+                                previus.musicData != current.musicData,
+                            builder: (context, stateData) {
+                              switch (stateData.musicData.status) {
+                                case Status.INITIAL:
+                                  return WidgetStateHandling(
+                                    assetsPath: "assets/images/podcast.png",
+                                    message: "Let's start find songs",
+                                  );
+                                case Status.LOADING:
+                                  return WidgetStateHandling(
+                                    assetsPath: "assets/images/podcast.png",
+                                    message: "Please wait...",
+                                  );
+                                case Status.COMPLETED:
+                                  final data = stateData.musicData.data ?? [];
+                                  if (data.isNotEmpty) {
+                                    return ListView.builder(
+                                      itemCount: data.length,
+                                      controller: _scrollController,
+                                      itemBuilder: (context, index) {
+                                        return ListMusicData(
+                                          indexMusic: index,
+                                          activePlaying: (() {
+                                            if (state is MusicPlayerPlaying &&
+                                                state.musicData.trackPreview ==
+                                                    stateData
+                                                        .musicData
+                                                        .data![index]
+                                                        .trackPreview) {
+                                              return true;
+                                            } else {
+                                              return false;
+                                            }
+                                          }()),
+                                          dataMusic:
+                                              stateData.musicData.data![index],
+                                          onPressSongSelected: () =>
+                                              blocMusicPlayer.startPlay(
+                                                  stateData
+                                                      .musicData.data![index],
+                                                  stateData.musicData.data!,
+                                                  index),
+                                        );
+                                      },
+                                    );
+                                  } else {
+                                    return WidgetStateHandling(
+                                        assetsPath:
+                                            "assets/images/error_data.png",
+                                        message:
+                                            "Sorry we can find anything..");
+                                  }
+                                case Status.ERROR:
+                                  return WidgetStateHandling(
+                                      assetsPath:
+                                          "assets/images/error_data.png",
+                                      message: stateData.musicData.message!);
+                                default:
+                                  return WidgetStateHandling(
+                                    assetsPath: "assets/images/podcast.png",
+                                    message: "Let's start find songs",
+                                  );
+                              }
+                            },
+                          ),
+                          (() {
+                            if (state is MusicPlayerPlaying) {
+                              return BottomPlayerSheet(
+                                  position: _position,
+                                  musicLength: _musicLength,
+                                  onPressNext: () {
+                                    if (state.indexData <
+                                        state.musicDataList.length) {
+                                      blocMusicPlayer.startPlay(
+                                          state.musicDataList[
+                                              state.indexData + 1],
+                                          state.musicDataList,
+                                          state.indexData + 1);
+                                    }
+                                  },
+                                  onPressPrevius: () {
+                                    if (state.indexData >= 1) {
+                                      blocMusicPlayer.startPlay(
+                                          state.musicDataList[
+                                              state.indexData - 1],
+                                          state.musicDataList,
+                                          state.indexData - 1);
+                                    }
+                                  },
+                                  musicDataList: state.musicDataList,
+                                  indexData: state.indexData,
+                                  playingMusic: playingMusic,
+                                  onPressPause: () {
+                                    if (playingMusic) {
+                                      _player.resume();
+                                    } else {
+                                      _player.pause();
+                                    }
+                                    setState(() {
+                                      playingMusic = !playingMusic;
+                                    });
+                                  },
+                                  musicData: state.musicData,
+                                  onSeekMusic: (value) {
+                                    int valueInt = value.round();
+                                    Duration newPos =
+                                        Duration(seconds: valueInt);
+                                    _player.seek(newPos);
+                                  });
+                            } else {
+                              return Container();
+                            }
+                          }())
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
